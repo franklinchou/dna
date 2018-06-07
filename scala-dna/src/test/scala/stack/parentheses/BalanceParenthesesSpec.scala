6440bca3df2b6e1c208758b1cf92a7796260e347
Franklin Chou <franklin@peeriq.com>
2018-06-07 00:08:01 -0400

    add balance parens with tests

--- scala-dna/src/test/scala/stack/parentheses/BalanceParenthesesSpec.scala
+++ scala-dna/src/test/scala/stack/parentheses/BalanceParenthesesSpec.scala
@@ -5,12 +5,18 @@ import org.scalatest.FunSpec
 class BalanceParenthesesSpec extends FunSpec {
 
   describe("Balance parentheses") {
-    val test = "1+2)*3-4)*5-6)))"
-    val expected = "( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )"
+
     it("should balance an expression") {
+      val test = "1+2)*3-4)*5-6)))"
+      val expected = "( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )"
       assert(BalanceParentheses(test) == expected)
     }
 
+    it("should balance an expression containing '^' ") {
+      val test = "9/c)+c*p^8+9))))"
+      val expected = "( ( 9 / c ) + ( c * ( p ^ ( 8 + 9 ) ) ) )"
+      assert(BalanceParentheses(test) == expected)
+    }
   }
 
 }
