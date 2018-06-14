fc167c26b32873f137c072fc11fd5305f8bd145a
Franklin Chou <franklin@peeriq.com>
2018-06-13 21:17:11 -0400

    stack more efficient than linked list?

--- scala-dna/src/test/scala/queue/QueueEfficiencySpec.scala
+++ scala-dna/src/test/scala/queue/QueueEfficiencySpec.scala
@@ -5,7 +5,11 @@ import utils.File
 
 class QueueEfficiencySpec extends FlatSpec {
 
+
+  // http://www.convertcsv.com/generate-test-data.htm
   def raw10000 = File.read("queue/test-10000.csv")
+
+  def raw1000000 = File.read("queue/test-1000000.csv")
 
   private def time(f: => Unit): BigDecimal = {
     val start = System.nanoTime()
@@ -30,15 +34,15 @@ class QueueEfficiencySpec extends FlatSpec {
   val qsD = new QueueStackDq[String]
 
 
-  /* "Enqueue" */ ignore should "run faster in QueueStackDq compared to QueueList" in {
-    val qlTime = time(enqueue(ql, raw10000))
-    val qsDTime = time(enqueue(qsD, raw10000))
-    assert(ql.size == 10000)
-    assert(qsD.size == 10000)
+  "Enqueue" should "run faster in QueueStackDq compared to QueueList" in {
+    val qlTime = time(enqueue(ql, raw1000000))
+    val qsDTime = time(enqueue(qsD, raw1000000))
     assert(qsDTime < qlTime)
   }
 
   "Dequeue" should "run faster in QueueList compared to QueueStackDq" in {
+    enqueue(ql, raw10000)
+    enqueue(qsD, raw10000)
     assert(ql.size == 10000)
     assert(qsD.size == 10000)
     val qlTime = time(dq(ql, raw10000))
