fea5c076e31b8b0083caa3eaf521a1470a212f8b
Franklin Chou <franklin@peeriq.com>
2018-06-17 17:55:28 -0400

    resolves #12

--- scala-dna/src/test/scala/stack/parentheses/DuplicateParenthesesSpec.scala
+++ scala-dna/src/test/scala/stack/parentheses/DuplicateParenthesesSpec.scala
@@ -7,19 +7,19 @@ class DuplicateParenthesesSpec extends FunSpec {
   describe("Duplicate parentheses") {
     val test1 = "((x+y))+z"
     it(s"given $test1, should find duplicates") {
-      val expected = "Duplicate parentheses found in sub-expression ((x+y))"
+      val expected = "Found duplicate parentheses"
       assert(DuplicateParentheses(test1) == expected)
     }
 
     val test2 = "(x+y)"
     it(s"given $test2, should find no duplicates") {
-      val expected = "No duplicate parentheses found"
+      val expected = "No duplicate parentheses"
       assert(DuplicateParentheses(test2) == expected)
     }
 
     val test3 = "((x+y)+((z)))"
-    ignore(s"given $test3, should find duplicates") {
-      val expected = "Duplicate parentheses found in sub-expression ((z))"
+    it(s"given $test3, should find duplicates") {
+      val expected = "Found duplicate parentheses"
       assert(DuplicateParentheses(test3) == expected)
     }
   }
