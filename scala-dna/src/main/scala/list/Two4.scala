a1fce048b160f8f17416341316a4c1c21d30388f
Franklin Chou <franklin@peeriq.com>
2018-06-21 12:49:34 -0400

    added recursive traversal and used linkedlist type alias

--- scala-dna/src/main/scala/list/Two4.scala
+++ scala-dna/src/main/scala/list/Two4.scala
@@ -2,48 +2,58 @@ package list
 
 import node.Node
 
+import scala.annotation.tailrec
+
 object Two4 extends App {
 
-  def partitionAbout(head: Option[Node[Int]], p: Int): Node[Int] = {
+  type LinkedList[T] = Option[Node[T]]
+
+  private def list2String(list: LinkedList[Int]): String = {
+    val b = StringBuilder.newBuilder
+
+    @tailrec
+    def traverse(l: LinkedList[Int]): Unit = l match {
+      case Some(Node(current, next)) =>
+        b.append(s"$current,")
+        traverse(next)
+      case None =>
+        b.append("null")
+    }
+
+    traverse(list)
+    b.toString()
+  }
+
+  def partitionAbout(list: LinkedList[Int], p: Int): Node[Int] = {
 
     val partition = Node(p, None) // create the new partition node
 
-    var h = head
     var greater = partition
     var less = partition
 
-    while (h.isDefined) {
-
-      if (h.get.data < p) {
-        // Prepend to list
-        val t = Node(h.get.data, Some(less))
-        less = t
-      } else if (h.get.data > p) {
-        // Append to list
-        val t = Node(h.get.data, None)
-        greater.next = Some(t)
-        greater = t
-      } else {
-        // If the value is equal to partition, do nothing
-      }
-      h = h.get.next
+    @tailrec
+    def traverse(l: LinkedList[Int]): Unit = l match {
+      case Some(Node(current, next)) =>
+        if (current < p) {
+          // Prepend to list
+          val t = Node(current, Some(less))
+          less = t
+        } else if (current > p) {
+          // Append to list
+          val t = Node(current, None)
+          greater.next = Some(t)
+          greater = t
+        } else {
+          // If the value is equal to partition, do nothing
+        }
+        traverse(next)
+      case None => ()
     }
+
+    traverse(list)
 
     // At the end of the function less is the new head
     less
-  }
-
-
-  private def list2String(head: Option[Node[Int]]): String = {
-    val b = StringBuilder.newBuilder
-    var h = head
-    while (h.isDefined) {
-      b.append(s"${h.get.data},")
-      h = h.get.next
-
-    }
-    b.append("null")
-    b.toString()
   }
 
   val test = Some(Node(5, Some(Node(6, Some(Node(4, Some(Node(2, Some(Node(3, Some(Node(1, None))))))))))))
@@ -53,5 +63,4 @@ object Two4 extends App {
 
   require(list2String(Some(r1)) == "1,3,2,4,6,5,7,null")
   require(list2String(Some(r2)) == "1,2,3,5,6,4,null")
-
 }
