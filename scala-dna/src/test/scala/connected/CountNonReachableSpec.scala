4a2c8b8f1d2f243a758fc594ef7490be5a7cdedf
Franklin Chou <franklin@peeriq.com>
2018-07-18 01:40:28 -0400

    automate tests

--- scala-dna/src/test/scala/connected/CountNonReachableSpec.scala
+++ scala-dna/src/test/scala/connected/CountNonReachableSpec.scala
@@ -5,32 +5,34 @@ import org.scalatest.FunSpec
 
 class CountNonReachableSpec extends FunSpec {
 
+  private def count(source: String, method: String, root: Int = 0): Int = {
+    val r = Relationships.load(source)
+    val size = r._1 // number of nodes in this graph
+    val graph = Relationships.makeGraph(r._2)
+    CountNonReachable.apply(method, root, size, graph)
+  }
+
+  // map of test file to expected
+  private val tests = {
+    Map(
+      "connected/test1.txt" -> 2,
+      "connected/test2.txt" -> 5
+    )
+  }
+
+
   describe("Count non-reachable nodes with bfs") {
-    it("test 1") {
-
-      val source = "connected/test1.txt"
-
-      val r = Relationships.load(source)
-
-      val size = r._1 // number of nodes in this graph
-      val graph = Relationships.makeGraph(r._2)
-
-      val c = CountNonReachable.apply("bfs", 0, size, graph)
-      assert(c == 2)
+    tests.foreach { t =>
+      val source = t._1
+      it(s"test $source") {
+        assert(count(source, "bfs") == t._2)
+      }
     }
+  }
 
-    it("test 2") {
 
-      val source = "connected/test2.txt"
+  describe("Count non-reachable nodes with dfs") {
 
-      val r = Relationships.load(source)
-
-      val size = r._1 // number of nodes in this graph
-      val graph = Relationships.makeGraph(r._2)
-
-      val c = CountNonReachable.apply("bfs", 0, size, graph)
-      assert(c == 5)
-    }
   }
 
 }
