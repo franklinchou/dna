a904b5c200831a3c8e7bdc8452aaf883b220620e
fmc <franklin@peeriq.com>
2018-07-22 15:10:14 -0400

    Data visualization for island-problem, size = 25 (#49)
    * 29
    
    * resolve #48
    
    * clean up
    
    * added coloring

---
+++ dataviz/make-table/controller.js
@@ -0,0 +1,91 @@
+function generate_table() {
+
+    // Renders the table from Island, size 25.
+    let tableToRender = [
+        [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+        [1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+        [1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0],
+        [0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+        [0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0],
+        [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1],
+        [0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+        [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+        [0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1],
+        [0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],
+        [1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1]
+    ]
+
+    let tableToRenderXSize = tableToRender.length;
+
+    let tableToRenderYSize = tableToRender[0].length;
+
+    // Grab the body tag
+    let body = document.getElementsByTagName("body")[0];
+
+    // Create the top level table element
+    let tbl = document.createElement("table");
+
+    // Create the top level table body element
+    let tblBody = document.createElement("body");
+
+    for (var i = 0; i < tableToRenderXSize; i++) {
+        var row = document.createElement("tr");
+
+        for (var j = 0; j < tableToRenderYSize; j++) {
+            var cell = document.createElement("td");
+
+            var cellId = `${i}-${j}`
+
+            cell.id = cellId;
+            
+            // Color scheme generated with: https://coolors.co
+            if (tableToRender[i][j] === 1) {
+                cell.setAttribute("bgcolor", "A3C9A8");
+            } else {
+                cell.setAttribute("bgcolor", "50808E");
+            }
+
+            cell.setAttribute("style", "padding:8px");
+            
+            var cellValue = document.createTextNode("");
+            
+            cell.appendChild(cellValue);
+            row.appendChild(cell);
+        }
+
+        // Append the created row to the table body
+        tblBody.appendChild(row);
+    }
+
+    tbl.appendChild(tblBody);
+    body.appendChild(tbl);
+
+    document.querySelector('table').addEventListener('click', function(event) {
+        if (event.target.getElementsByTagName("td")) {
+            var targetCellId = event.target.id;
+            var targetCell = document.getElementById(targetCellId);
+        
+            if (targetCell.getAttribute("bgcolor") === "A3C9A8") {
+                targetCell.setAttribute("bgcolor", "BB7E8C");
+            } else if (targetCell.getAttribute("bgcolor") === "BB7E8C") {
+                targetCell.setAttribute("bgcolor", "A3C9A8");
+            } else {
+                // If water, do nothing
+            }
+        }
+    });
+}
--- scala-dna/src/test/scala/queue/IslandSpec.scala
+++ scala-dna/src/test/scala/queue/IslandSpec.scala
@@ -22,13 +22,17 @@ class IslandSpec extends FunSpec {
   }
 
   describe("Find islands in a static file") {
-    it("should find two islands") {
+    it("should find two islands in 5x5 grid") {
       val m = makeTestMatrix("island/5.csv")
       assert(Island.countIslands(m) == 2)
     }
-    it("should find five islands") {
+    it("should find five islands in 10x10 grid") {
       val m = makeTestMatrix("island/10.csv")
       assert(Island.countIslands(m) == 5)
+    }
+    it("should find 7 islands in 25x25 grid") {
+      val m = makeTestMatrix("island/25.csv")
+      assert(Island.countIslands(m) == 7)
     }
   }
 
