d12e7c003fe22a42f4f8d43e8fe0c0535b7cf07b
Franklin Chou <franklin@peeriq.com>
2018-07-23 07:49:21 -0400

    saving progress

--- scala-dna/src/main/scala/node/graphs/connected/MaxConnected.scala
+++ scala-dna/src/main/scala/node/graphs/connected/MaxConnected.scala
@@ -3,7 +3,26 @@ package node.graphs.connected
 import node.Node.World
 import utils.Point
 
+import scala.collection.mutable
+
+
+/**
+  * Immutable queue returns the dequeued element and the new queue with that
+  * element removed.
+  */
+
+import scala.collection.mutable.Queue
+
 object MaxConnected {
+
+  private def moves(p: Point): Seq[Point] = {
+    Seq(
+      Point(p.x + 1, p.y),
+      Point(p.x - 1, p.y),
+      Point(p.x, p.y + 1),
+      Point(p.x, p.y - 1)
+    )
+  }
 
 
   def apply(world: World): Int = {
@@ -12,26 +31,50 @@ object MaxConnected {
 
     val worldVSize = world(0).length // vertical world size (y)
 
+    var largest = 0
+
+    val isMoveable = (p: Point) => {
+      (p.x >= 0 && p.y >= 0) && (p.x < worldHSize && p.y < worldVSize)
+    }
 
     for {
       x <- 0 until worldHSize
       y <- 0 until worldVSize
     } {
       val source = Point(x, y)
+      val sourceColor = world(x)(y)
+
+      val size = 0
+
+      val q = Queue[Point]()
+      val visited = new mutable.HashSet[Point]()
+
+      q.enqueue(source)
+
+      while (q.nonEmpty) {
+
+        val p = q.dequeue
 
 
-    
+        /**
+          * Add moves only if they contain a same color element
+          */
+        for (m <- moves(source)) {
+          if (isMoveable(m) && world(m.x)(m.y) == sourceColor) {
+            q.enqueue(m)
+          }
+        }
+      }
+
+
+      Math.max(size, largest)
+
     }
 
-
-    0
+    largest
   }
 
 }
 
 
-case class MaxConnected(world: World) {
-
-
-
-}
+case class MaxConnected(world: World)
